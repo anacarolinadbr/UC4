d70c635c5dca5e2dd69ed5837a488fc73f187d55
anacarolinadbr <anacarolinadbr06@gmail.com>
2025-08-22 19:12:14 -0300

    Arquivos

--- trabalhoHemocentro/src/Pessoa.ts
+++ trabalhoHemocentro/src/Pessoa.ts
@@ -1,24 +1,30 @@
 export class Pessoa {
+    //Declarando atributos para a classe
     protected nome: string
     protected idade: number
     protected peso: number
+    //Protected - só pode acessar nesta classe ou nas classes filhas 
     
 
+    //Constructor para criar uma nova pessoa e vai receber os três valores 
     constructor(nome:string, idade:number, peso:number){
         this.nome = nome
         this.idade = idade
         this.peso = peso
     }
 
+    //Método vai retorar uma string com as informações da pessoa
     mostrarInfo():string {
         return `Nome: ${this.nome}, Idade: ${this.idade}, Peso: ${this.peso}`
 
     }
 
+    //Método getNome é para "pegar" o valor do atributo nome
     getNome():string{
         return this.nome
     }
      
+    //Método setNome é para alterar o valor da variavel nome 
     setNome(novoNome:string): void{
         this.nome = novoNome 
     }
